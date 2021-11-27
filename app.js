003963582bb045953bb0df570fc451d7c6c7bde2
Juan Luis Chaurant <juanluischaurant@gmail.com>
2021-11-27 16:32:31 -0400

    almost done here

--- app.js
+++ app.js
@@ -9,18 +9,22 @@ let parametersQR = {
 
 let parameters
 
-let btn = document.querySelector("button")
-let downloadBtn = document.querySelector('.download-button')
+let btn = document.querySelector(".generate-btn")
+let downloadBtn = document.querySelector('.download-btn')
 let img = document.querySelector("img")
 let information = document.querySelector(".information")
 let format = document.querySelector('#format')
 let padding = document.querySelector('.padding')
-let qrColorPicker = document.querySelector('.color-picker')
+let qrColorPicker = document.querySelector('.qr-color-picker')
+let backColorPicker = document.querySelector('.back-color-picker')
 let toggleTheme = document.querySelector('#checkbox-toggle-theme')
 
 toggleTheme.addEventListener('change', () => {
   document.body.classList.toggle('dark')
-  console.log(document)
+  // console.log(document)
+
+  downloadBtn.classList.toggle('dark-btn')
+  btn.classList.toggle('dark-btn')
 })
 
 qrColorPicker.addEventListener('change', () => {
@@ -29,6 +33,13 @@ qrColorPicker.addEventListener('change', () => {
   // console.log('well, hello there')
   // console.log(qrColorPicker.value)
 })
+
+backColorPicker.addEventListener('change', () => {
+  let color = backColorPicker.value.slice(1)
+
+  parametersQR.backgroundColor = color
+})
+
 btn.addEventListener("click", () => {
   parametersQR.data = information.value || "juanluischaurant.com"
   parametersQR.format = format.value
@@ -46,11 +57,15 @@ btn.addEventListener("click", () => {
 const loadDefaultUI = () => {
   padding.value = parametersQR.padding
   information.value = parametersQR.data
-  img.src = 'https://api.qrserver.com/v1/create-qr-code/?size=250&bgcolor=38-38-38&color=255-255-255&qzone=1&data=juanluischaurant.com&format=svg'
+  img.src = `https://api.qrserver.com/v1/create-qr-code/?size=250&bgcolor=38-38-38&color=255-255-255&qzone=1&data=juanluischaurant.com&format=svg`
 
+  downloadBtn.href = img.src
+  
   // console.log('general Kenobi')
   // toggleTheme.checked = false
   toggleTheme.checked && document.body.classList.add('dark')
+  toggleTheme.checked && btn.classList.toggle('dark-btn')
+  toggleTheme.checked && downloadBtn.classList.toggle('dark-btn')
 }
 
 loadDefaultUI()
